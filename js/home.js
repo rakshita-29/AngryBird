c989b52a7b9a3725f5b25e17dbd3c21f73aac10e
Rakshita <rakshitachoudhary2000@gmail.com>
2023-10-06 14:47:01 +0530

    ADD:sound effects on landing page

--- js/home.js
+++ js/home.js
@@ -1,1 +1,28 @@
-// Home Content
+// Function to toggle audio playback
+var audio = new Audio('../sounds/theme_song.mp3');
+var isPlaying = false;
+
+function toggleAudio() {
+    if (isPlaying) {
+        audio.pause();
+    } else {
+        audio.play();
+    }
+    
+    isPlaying = !isPlaying;
+}
+document.getElementById('sound_btn').addEventListener('click', toggleAudio);
+
+//click sound for button
+var clickSound = new Audio('../sounds/click-sound.wav');
+
+var buttons = document.querySelectorAll('.btn');
+
+buttons.forEach(function (button) {
+    button.addEventListener('click', function () {
+
+        clickSound.currentTime = 0; 
+        clickSound.play();
+
+    });
+});

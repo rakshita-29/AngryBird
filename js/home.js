cc548d84dd336e45603138c864a85c90b57ac0ff
yash114bansal <yash114bansal@gmail.com>
2023-10-07 10:03:16 +0530

    add button toggle

--- js/home.js
+++ js/home.js
@@ -1,14 +1,17 @@
-// Function to toggle audio playback
 var audio = new Audio('../sounds/theme_song.mp3');
 var isPlaying = false;
 
+const volume_toggele_btn = document.getElementById("volume-span")
 function toggleAudio() {
     if (isPlaying) {
+        volume_toggele_btn.innerHTML = "volume_off";
+
         audio.pause();
     } else {
+        volume_toggele_btn.innerHTML = "volume_up";
         audio.play();
     }
-    
+
     isPlaying = !isPlaying;
 }
 document.getElementById('sound_btn').addEventListener('click', toggleAudio);
@@ -21,7 +24,7 @@ var buttons = document.querySelectorAll('.btn');
 buttons.forEach(function (button) {
     button.addEventListener('click', function () {
 
-        clickSound.currentTime = 0; 
+        clickSound.currentTime = 0;
         clickSound.play();
 
     });

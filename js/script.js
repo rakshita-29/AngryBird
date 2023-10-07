dc62c7d0b051290326e74a265a95fa74906784dd
yash114bansal <yash114bansal@gmail.com>
2023-10-07 09:47:39 +0530

    change cursor

--- js/script.js
+++ js/script.js
@@ -1,20 +1,32 @@
 function floatElements() {
-    const elements = document.querySelectorAll('.floating-element');
-    elements.forEach((element, index) => {
-      const yOffset = 10; 
-      const speed = 2500; 
-     
+  const elements = document.querySelectorAll('.floating-element');
+  elements.forEach((element, index) => {
+    const yOffset = 10;
+    const speed = 2500;
 
-      setInterval(() => {
+    setInterval(() => {
+      element.style.transition = 'transform 1.5s ease-in-out';
+      element.style.transform = `translateY(${yOffset}px)`;
+      setTimeout(() => {
         element.style.transition = 'transform 1.5s ease-in-out';
-        element.style.transform = `translateY(${yOffset}px)`;
-        setTimeout(() => {
-          element.style.transition = 'transform 1.5s ease-in-out';
-          element.style.transform = 'translateY(0)';
-        }, speed / 2);
-      }, speed );
-    });
-  }
+        element.style.transform = 'translateY(0)';
+      }, speed / 2);
+    }, speed);
+  });
+}
 
- 
-  window.addEventListener('load', floatElements);
+window.addEventListener('load', floatElements);
+
+const customCursor = document.getElementById('custom-cursor');
+
+document.addEventListener('mousemove', (e) => {
+  customCursor.style.left = `${e.clientX}px`;
+  customCursor.style.top = `${e.clientY}px`;
+});
+
+document.addEventListener('mousedown', (e) => {
+  customCursor.style.backgroundImage = `url("../images/hand2.png")`;
+})
+document.addEventListener('mouseup', () => {
+  customCursor.style.backgroundImage = `url("../images/hand1.png")`;
+});

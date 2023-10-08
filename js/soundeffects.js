e31ca01cf7eff549c671df18d9a90102f206ef31
Rakshita <rakshitachoudhary2000@gmail.com>
2023-10-08 22:44:16 +0530

    try to fix sound effect bug

--- js/soundeffects.js
+++ js/soundeffects.js
@@ -1,28 +1,55 @@
 const mycanvas = document.getElementById("canvas");
 
-
+let birdSelected = null;
+let birdShoot = null;
 const selectSound = new Audio("./sounds/select.mp3");
 const dragSound = new Audio("../sounds/strech.mp3");
 const releaseSound = new Audio("../sounds/release.mp3");
 
-let birdSelected = false;
-
-mycanvas.addEventListener("mousedown", () => {
-  birdSelected = true;
-  playSound(selectSound);
-});
-
-mycanvas.addEventListener("mousemove", () => {
-  if (birdSelected) {
-    playSound(dragSound);
+mycanvas.addEventListener("mousedown", (e) => {
+  const mouseX = e.clientX - mycanvas.getBoundingClientRect().left;
+  const mouseY = e.clientY - mycanvas.getBoundingClientRect().top;
+  if (birdSelected != null) {
+    if (
+      mouseX >= birdSelected.position.x &&
+      mouseX <= birdSelected.position.x + birdSelected.width &&
+      mouseY >= birdSelected.position.y &&
+      mouseY <= birdSelected.position.y + birdSelected.height
+    ) {
+      birdShoot = birdSelected;
+      playSound(selectSound);
+    }
   }
 });
 
-mycanvas.addEventListener("mouseup", () => {
-  if (birdSelected) {
-    birdSelected = false;
+mycanvas.addEventListener("mousemove", (e) => {
+  const mouseX = e.clientX - mycanvas.getBoundingClientRect().left;
+  const mouseY = e.clientY - mycanvas.getBoundingClientRect().top;
+
+  if (birdSelected === null) {
+    for (const bird of birds) {
+      if (
+        mouseX >= bird.position.x &&
+        mouseX <= bird.position.x + bird.width &&
+        mouseY >= bird.position.y &&
+        mouseY <= bird.position.y + bird.height &&
+        !isSlingShotActive
+      ) {
+        bird.position.x = 170;
+        bird.position.y = ground - slingShotHeight;
+        birdSelected = bird;
+        playSound(dragSound);
+      }
+    }
+  }
+});
+
+mycanvas.addEventListener("mouseup", (e) => {
+  if (birdShoot != null) {
+
     playSound(releaseSound);
   }
+  birdSelected = null;
 });
 
 function playSound(sound) {
